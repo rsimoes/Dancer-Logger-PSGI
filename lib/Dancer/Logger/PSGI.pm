d5e13787a99e9c1d45bd0b0f6ed4efd036ed7532
Richard Simões <rsimoes@cpan.org>
2011-12-24 00:26:05 -0600

    bring over POD changes from 0.03

--- lib/Dancer/Logger/PSGI.pm
+++ lib/Dancer/Logger/PSGI.pm
@@ -30,25 +30,32 @@ sub _log {
 
 =head1 SYNOPSIS
 
-In your Dancer's configuration file:
+In your Dancer's environment file:
 
     logger: PSGI
+    - plack_middlewares:
+      -
+        - ConsoleLogger
 
 In your application
 
     warning "this is a warning"
-
-Then, in your app.psgi
-
-    $app = builder { enable "ConsoleLogger"; $app; }
-
-or in your environment file:
-
-    - plack_middlewares:
-      - ConsoleLogger
 
 With L<Plack::Middleware::ConsoleLogger>, all your log will be send to the JavaScript console of your browser.
 
 =head1 DESCRIPTION
 
 This class is an interface between your Dancer's application and B<psgix.logger>. Message will be logged in whatever logger you decided to use in your L<Plack> handler. If no logger is defined, nothing will be logged.
+
+=head1 AUTHOR
+
+franck cuny <franck@lumberjaph.net>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2010 by franck cuny.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
